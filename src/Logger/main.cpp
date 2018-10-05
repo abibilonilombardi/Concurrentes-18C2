ca30a4c50eb14356589a7348aa9a605782d4c2f8
AyeTheBest <abibiloni@fi.uba.ar>
2018-10-04 23:54:23 -0300

    Adding inital version of Logger and Lock

---
+++ src/Logger/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "Logger.h"
+#include "LogMessages.cpp"  //ver si es h o cpp
+
+
+
+using namespace std;
+
+int main () {
+
+    Logger* logger = Logger::getInstance();
+
+    pid_t pid = getpid();
+
+    for (int i; i<1000; i++){
+        if (pid!=0)
+            pid = fork();
+    }
+
+    cout << "pid: "<< getpid() << endl; 
+    logger->log(CREACION_BARCO_EXITO);
+
+    sleep(5);
+
+    Logger::destroy();
+
+	return 0;
+}
